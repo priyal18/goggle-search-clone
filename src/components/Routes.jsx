d3405cf30b209235ed4f1960ae08963e2f71a0dd
priyal18 <priyal930gupta@gmail.com>
2022-01-02 23:41:22 +0530

    SEARCH,news,images

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route, Navigate } from 'react-router-dom';
 
-export const Routes = () => {
+import {Results} from './Results';
+
+export const Routess = () => {
     return (
-        <div>
-            <h1>Routes</h1>
+        <div className="p-6">
+            <Routes>
+                <Route exact path="/" element={<Navigate to="/search" />}/>
+                <Route exact path="/search" element={<Results/>}/>
+                <Route exact path="/images" element={<Results/>}/>
+                <Route exact path="/videos" element={<Results/>}/>
+                <Route exact path="/news" element={<Results/>}/>
+
+            </Routes>
+            
         </div>
     )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Search = () => {
+    return (
+        <div>
+            <h1>SEARCH</h1>
+        </div>
+    )
+}
